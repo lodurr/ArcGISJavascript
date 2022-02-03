2db553d3312e1a59bd25b56030c485e943854e69
Diego Cordero <ing.diegocordero@gmail.com>
2022-02-03 11:04:19 -0400

    Exercise1Volume
    Exercise1Volume

---
+++ Ejercicio1Volumen/myscript.js
@@ -0,0 +1,17 @@
+const volumen = 16*23*13;
+console.log('volumen', volumen)
+
+const areaLateral = 23*13;
+const areaBase = 16*23;
+const areaTotal = areaLateral + 2*areaBase;
+console.log ('El area Total es:', areaTotal + 'cm2')
+
+const base = 23;
+const altura = 13;
+const ancho = 16;
+
+const volumen2 = base * altura * ancho;
+console.log('volumen2', volumen2)
+
+const areaTotal2 = base * altura + 2*base * ancho;
+console.log ('El area Total es:', areaTotal2 + 'cm2')

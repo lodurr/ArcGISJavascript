6811d37c6240112fcb76b8c20e527e0090a8a011
Diego Cordero <ing.diegocordero@gmail.com>
2022-02-03 18:14:39 -0400

    Ejercicio9 Filter Pares
    Ejercicio9 Filter Pares

---
+++ Ejercicio9_FilterPares/myscript.js
@@ -0,0 +1,5 @@
+const number = [253, 8575, 1, 20, 562, 1233, 25, 27, 258, 254, 7485, 2683];
+
+const pares = number.filter(par => par%2==0)
+const impares = number.filter(impar => impar%2==1)
+alert("Los numeros pares son: " + pares+  " \n  Los numeros impares son: " + impares)

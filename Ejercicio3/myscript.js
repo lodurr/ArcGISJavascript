1d1ca3ee5eff1d8ed350bf19d2b3c5237c372344
Diego Cordero <ing.diegocordero@gmail.com>
2022-02-03 12:06:43 -0400

    Ejercicio3
    Ejercicio3

---
+++ Ejercicio3/myscript.js
@@ -0,0 +1,23 @@
+const data = ["hola", 2, 5, "adios"];
+if (data[1] > data[2]){
+    console.log(data[1] + ' Es Mayor que ' + data[2] )
+}
+else{
+    console.log(data[2] + ' Es Mayor que ' + data [1])
+}
+suma = data[1]+data[2];
+resta= data[1]-data[2];
+multi= data[1]*data[2];
+divi = data[1]/data[2];
+resto= data[1]%data[2]
+
+
+console.log('La suma de los valores numericos es: ' + suma);
+console.log('La resta de los valores numericos es: ' + resta);
+console.log('La multiplicacion de los valores numericos es: ' + multi);
+console.log('La division de los valores numericos es: ' + divi);
+console.log('El restante de los valores numericos es: ' + resto);
+
+
+
+
